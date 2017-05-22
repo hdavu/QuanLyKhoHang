bf64ef11fd58bf9fbc89d736113c9ce9a269145c
Edward196 <edwardnguyen196@gmail.com>
2017-05-23 00:58:15 +0700

    Tìm kiếm + show
    Tìm kiếm vad show

--- ThucTap5_QuanLyKhoHang/Program.cs
+++ ThucTap5_QuanLyKhoHang/Program.cs
@@ -16,7 +16,7 @@ namespace ThucTap5_QuanLyKhoHang
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormDangNhap());
+            Application.Run(new FormMain());
         }
     }
 }
